8dcf79a78ce83bf1ce4d5702086eb80f037f6809
Jared M. Peterson <jared@peteyinc.com>
2016-05-27 15:46:00 -0600

    app4 edits

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -1,7 +1,5 @@
 angular.module("apiApp").controller("starControl", function($scope, starWars) {
 
-  $scope.name = "Jared";
-
   $scope.getPeople = function() {
     starWars.getPeople().then(function(starWarsData) {
       $scope.people = starWarsData;
